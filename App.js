b4f84b1ae244f47ce86a479f80383b6b8c4c9cd5
Daniel Santos <daniel17sants@gmail.com>
2020-05-04 13:57:45 -0300

    Custom Fonts

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+import About from './screens/about';
+import ReviewDetails from './screens/reviewDetails';
+import { AppLoading } from 'expo';
+
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'ninuto-bold': require('./assets/fonts/Nunito-Bold.ttf')
+});
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+  const [fontsLoaded, setFonstLoaded] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFonstLoaded(true)}
+      />
+    );
+  }
+
+}
