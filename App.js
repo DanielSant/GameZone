99d8c519b47a795140af03a8d38f17a0e1fde6ec
Daniel Santos <daniel17sants@gmail.com>
2020-05-05 13:56:17 -0300

    Stack navigator

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import React, { useState } from 'react';
 import * as Font from 'expo-font';
 import Home from './screens/home';
-import About from './screens/about';
-import ReviewDetails from './screens/reviewDetails';
 import { AppLoading } from 'expo';
+import Navigator from './routes/homeStack';
 
 const getFonts = () => Font.loadAsync({
   'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
   'ninuto-bold': require('./assets/fonts/Nunito-Bold.ttf')
 });
 
-
-
 export default function App() {
   const [fontsLoaded, setFonstLoaded] = useState(false);
 
   if (fontsLoaded) {
     return (
-      <Home />
+      <Navigator />
     );
   } else {
     return (
